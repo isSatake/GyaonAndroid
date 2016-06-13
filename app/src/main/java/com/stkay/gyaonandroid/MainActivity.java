56d19384d4bcea439b6f8c784d427c677d9b17aa
Hiroaki Satake <stkayy@gmail.com>
2016-06-13 23:20:19 +0900

    録音できる

--- app/src/main/java/com/stkay/gyaonandroid/MainActivity.java
+++ app/src/main/java/com/stkay/gyaonandroid/MainActivity.java
@@ -1,13 +1,105 @@
 package com.stkay.gyaonandroid;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.media.MediaRecorder;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.jar.Manifest;
 
 public class MainActivity extends AppCompatActivity {
+    final String TAG = "MainActivity";
+    MediaRecorder mediaRecorder = new MediaRecorder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this, "android.permission.RECORD_AUDIO") != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, "android.permission.WRITE_EXTERNAL_STORAGE") != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, "android.permission.INTERNET") != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{"android.permission.RECORD_AUDIO",
+                    "android.permission.WRITE_EXTERNAL_STORAGE",
+                    "android.permission.INTERNET"}, 0);
+        }
+        findViewById(R.id.rec_button).setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    Log.d(TAG, "onActionDown");
+                    recStart();
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    Log.d(TAG, "onActionUp");
+                    recStop();
+                }
+                return true;
+            }
+        });
+    }
+
+    private void recStart() {
+        Log.d(TAG, "recStart");
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd kk:mm:ss");
+        String date = df.format(new Date());
+        String path = Environment.getExternalStorageDirectory() + "/gyaon-wav/" + date + ".wav";
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.DEFAULT);
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
+        mediaRecorder.setOutputFile(path);
+        try {
+            mediaRecorder.prepare();
+            mediaRecorder.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //upload wav to gyaon server
+    }
+
+    private void recStop() {
+        Log.d(TAG, "recStop");
+        mediaRecorder.stop();
+        mediaRecorder.reset();
+        if(checkNetworkStatus()) {
+            upload();
+        }
+    }
+
+    private Boolean checkNetworkStatus() {
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null) {
+            Log.d(TAG, "network connected");
+            return networkInfo.isConnected();
+        } else {
+            Log.d(TAG, "network disconnected");
+            return false;
+        }
+    }
+
+    private void upload() {
+        //check gyaonID
+    }
+
+    private class uploadTask extends AsyncTask<String, Void, String>{
+        //send gyaonID as cookie
+
     }
 }
